651a8e87b1b1d362ee28b47ad7c7676cba061f0f
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-17 18:07:53 +0530

    Add random users public api endpoints
    - Fix typos
    - Fix error middleware variable bug
    - Add random users json

--- src/app.js
+++ src/app.js
@@ -20,10 +20,13 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 // api routes
-import healthcheckRouter from "./routes/healthcheck.routes.js";
 import { errorHandler } from "./middlewares/error.middlewares.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import randomuserRouter from "./routes/public/randomuser.routes.js";
 
 app.use("/api/v1/healthcheck", healthcheckRouter);
+
+app.use("/api/v1/public/randomusers", randomuserRouter);
 
 // common error handling middleware
 app.use(errorHandler);
---
+++ src/routes/public/randomuser.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+import {
+  getARandomUser,
+  getRandomUsers,
+} from "../../controllers/public/randomuser.controllers.js";
+
+const router = Router();
+
+router.route("/").get(getRandomUsers);
+router.route("/user").get(getARandomUser);
+
+export default router;

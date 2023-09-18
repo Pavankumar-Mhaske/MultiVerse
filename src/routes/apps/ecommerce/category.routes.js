a60922944e0686867a07b0848bc6a5a00868dfd3
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-18 23:40:04 +0530

    Add CRUD functionality for category with validators

--- src/routes/apps/ecommerce/category.routes.js
+++ src/routes/apps/ecommerce/category.routes.js
@@ -2,8 +2,16 @@ import { Router } from "express";
 import {
   createCategory,
   getAllCategories,
+  deleteCategory,
+  updateCategory,
+  getCategory,
 } from "../../../controllers/apps/ecommerce/category.controllers.js";
-import { createCategoryValidator } from "../../../validators/ecommerce/category.validators.js";
+
+import {
+  createCategoryValidator,
+  categoryPathVariableValidator,
+  updateCategoryValidator,
+} from "../../../validators/ecommerce/category.validators.js";
 import { validate } from "../../../validators/validate.js";
 import { isAdmin, verifyJWT } from "../../../middlewares/auth.middlewares.js";
 
@@ -13,5 +21,23 @@ router
   .route("/")
   .post(verifyJWT, isAdmin, createCategoryValidator(), validate, createCategory)
   .get(getAllCategories);
+
+router
+  .route("/:categoryId")
+  .get(categoryPathVariableValidator(), validate, getCategoryById)
+  .delete(
+    verifyJWT,
+    isAdmin,
+    categoryPathVariableValidator(),
+    validate,
+    deleteCategory
+  )
+  .patch(
+    verifyJWT,
+    isAdmin,
+    updateCategoryValidator(),
+    validate,
+    updateCategory
+  );
 
 export default router;

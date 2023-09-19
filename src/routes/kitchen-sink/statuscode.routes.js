6ca3e0bee70420c979f8e7a1fe2fd847071de9fa
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-19 19:35:27 +0530

    Add get all status codes api

--- src/routes/kitchen-sink/statuscode.routes.js
+++ src/routes/kitchen-sink/statuscode.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { getStatusCode } from "../../controllers/kitchen-sink/statuscode.controllers.js";
+import {
+  getAllStatusCodes,
+  getStatusCode,
+} from "../../controllers/kitchen-sink/statuscode.controllers.js";
 import { statusCodeValidator } from "../../validators/kitchen-sink/statuscode.validators.js";
 
 import { validate } from "../../validators/validate.js";
 
 const router = Router();
+
+router.route("/").get(getAllStatusCodes);
 
 router
   .route("/:statusCode")

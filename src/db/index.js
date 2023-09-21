526030ffa680fc7944b4b9211a918a0c295b7f2c
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-21 11:52:06 +0530

    Update README.md with updated docker command. Add formatting to the console logs

--- src/db/index.js
+++ src/db/index.js
@@ -6,13 +6,17 @@ export let dbInstance = undefined;
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
     // NOTE: command to drop mongo database
     // console.log(connectionInstance.connection.db.dropDatabase({
     //   dbName:"apihub"
     // }));
     dbInstance = connectionInstance;
-    console.log(`MongoDB Connected: ${connectionInstance.connection.host}`);
+    console.log(
+      `\n☘️  MongoDB Connected! Db host: ${connectionInstance.connection.host}`
+    );
   } catch (error) {
     console.log(`MongoDB connection error : ${error}`);
     process.exit(1);

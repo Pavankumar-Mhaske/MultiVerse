d0bb25d7c15f6a5cdd322754627a73d96264d712
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-21 17:01:31 +0530

    fix(chat-app): fix validation error messages

--- src/validators/apps/chat-app/chat.validators.js
+++ src/validators/apps/chat-app/chat.validators.js
@@ -25,13 +25,13 @@ const participantIdPathVariableValidator = () => {
     param("participantId")
       .notEmpty()
       .isMongoId()
-      .withMessage("Invalid user id"),
+      .withMessage("Invalid participant id"),
   ];
 };
 
 const chatIdPathVariableValidator = () => {
   return [
-    param("chatId").notEmpty().isMongoId().withMessage("Invalid user id"),
+    param("chatId").notEmpty().isMongoId().withMessage("Invalid chat id"),
   ];
 };
 

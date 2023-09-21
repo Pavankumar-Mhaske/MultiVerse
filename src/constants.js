fb744f415c3b119784d62b8aa2a49cfd9c9c0037
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-21 23:29:05 +0530

    feat(chat-app): add and implement socket.io - Add socket io and done setup - Add events related to setup, connection, message, chats and typing - Add server side socket events on new chat aded and new message received - Move socket events to the constant file

--- src/constants.js
+++ src/constants.js
@@ -5,9 +5,7 @@ export const UserRolesEnum = {
   ADMIN: "ADMIN",
   USER: "USER",
 };
-
 export const AvailableUserRoles = Object.values(UserRolesEnum);
-
 /**
  * @type {{ PENDING: "PENDING"; CANCELLED: "CANCELLED"; DELIVERED: "DELIVERED" } as const}
  */
@@ -16,9 +14,7 @@ export const OrderStatusEnum = {
   CANCELLED: "CANCELLED",
   DELIVERED: "DELIVERED",
 };
-
 export const AvailableOrderStatuses = Object.values(OrderStatusEnum);
-
 /**
  * @type {{ UNKNOWN:"UNKNOWN"; RAZORPAY: "RAZORPAY"; PAYPAL: "PAYPAL"; } as const}
  */
@@ -27,9 +23,7 @@ export const PaymentProviderEnum = {
   RAZORPAY: "RAZORPAY",
   PAYPAL: "PAYPAL",
 };
-
 export const AvailablePaymentProviders = Object.values(PaymentProviderEnum);
-
 /**
  * @type {{ FLAT:"FLAT"; } as const}
  */
@@ -37,9 +31,7 @@ export const CouponTypeEnum = {
   FLAT: "FLAT",
   // PERCENTAGE: "PERCENTAGE",
 };
-
 export const AvailableCouponTypes = Object.values(CouponTypeEnum);
-
 /**
  * @type {{ GOOGLE: "GOOGLE"; GITHUB: "GITHUB"; EMAIL_PASSWORD: "EMAIL_PASSWORD"} as const}
  */
@@ -48,9 +40,7 @@ export const UserLoginType = {
   GITHUB: "GITHUB",
   EMAIL_PASSWORD: "EMAIL_PASSWORD",
 };
-
 export const AvailableSocialLogins = Object.values(UserLoginType);
-
 /**
  * @type {{ MOST_VIEWED: "mostViewed"; MOST_LIKED: "mostLiked"; LATEST: "latest"; OLDEST: "oldest"} as const}
  */
@@ -60,17 +50,25 @@ export const YouTubeFilterEnum = {
   LATEST: "latest",
   OLDEST: "oldest",
 };
-
 export const AvailableYouTubeFilters = Object.values(YouTubeFilterEnum);
-
 export const USER_TEMPORARY_TOKEN_EXPIRY = 20 * 60 * 1000; // 20 minutes
-
 export const MAXIMUM_SUB_IMAGE_COUNT = 4;
 export const MAXIMUM_SOCIAL_POST_IMAGE_COUNT = 6;
-
-// ? REFACTOR: change name "apihub" everywhere in the codebase to "freeapi" (low priority)
-export const DB_NAME = "multiverse";
-
+export const DB_NAME = "freeapi";
 export const paypalBaseUrl = {
   sandbox: "https://api-m.sandbox.paypal.com",
+};
+
+/**
+ * @description set of events that we are using in chat app. more to be added as we develop the chat app
+ */
+export const ChatEventEnum = {
+  SETUP_EVENT: "setup", // ? get socket ready for user
+  CONNECTED_EVENT: "connected", // ? once user is ready to go
+  JOIN_CHAT_EVENT: "joinChat", // ? when new chat or group chat get's added
+  NEW_CHAT_EVENT: "newChat", // ? when user selects an existing user to chat with
+  TYPING_EVENT: "typing", // ? when participant starts typing
+  STOP_TYPING_EVENT: "stopTyping", // ? when participant stops typing
+  MESSAGE_RECEIVED_EVENT: "messageReceived", // ? when new message is received
+  DISCONNECT_EVENT: "disconnect", // ? when user gets disconnected
 };

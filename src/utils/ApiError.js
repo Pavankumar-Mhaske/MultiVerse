b5d2ac0131d7f347cc249b604eddb8d06f4faaaa
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-23 22:57:19 +0530

    test(server) : testing the ApiError.js file

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,5 +1,4 @@
 import { errorHandler } from "../middlewares/error.middlewares.js";
-
 /**
  * @description Common Error class to throw an error from anywhere.
  * The {@link errorHandler} middleware will catch this error at the central place and it will return an appropriate response to the client

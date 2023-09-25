272b05b66f8aff5ff23208bc99d84dd58bca4a83
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-25 09:49:54 +0530

    test(server) : testing the file multer.middleware.js file...

--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -1,10 +1,23 @@
 import multer from "multer";
 
+/**
+ * multer.diskStorage() is a method provided by the multer library for configuring the storage engine used to store uploaded files.
+ * types of the storage engines:
+ * 1. Disk storage engine
+ * 2. Memory storage engine
+ * 3. Cloud storage engines
+ * 4. Database storage engines
+ */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // This storage needs public/images folder in the root directory
     // Else it will throw an error saying cannot find path public/images
+    // TODO: Add try catch block to handle the error
     cb(null, "./public/images");
+    /**
+     * The cb function is used to indicate that the destination directory has been determined,
+     *  and to pass that information back to multer. The cb function takes two arguments: an error object (or null if there is no error), and the destination directory.
+     */
   },
   // Store file in a .png/.jpeg/.jpg format instead of binary
   filename: function (req, file, cb) {
@@ -13,12 +26,21 @@ const storage = multer.diskStorage({
       fileExtension = file.originalname.substring(
         file.originalname.lastIndexOf(".")
       );
+      /**
+       * For example, if file.originalname is "my-picture.jpg" and file.originalname.lastIndexOf(".") returns 10 (indicating that the last dot is at index 10),
+       * then file.originalname.substring(10) would extract the substring starting from index 10 to the end of the string, resulting in ".jpg."
+       */
     }
-    const filenameWithoutExtension = file.originalname
-      .toLowerCase()
-      .split(" ")
-      .join("-")
-      ?.split(".")[0];
+
+    const filenameWithoutExtension = file.originalname // Get the original filename
+      .toLowerCase() // Convert the filename to lowercase
+      .split(" ") // Split it by spaces
+      .join("-") // Join the parts with hyphens
+      ?.split(".")[0]; // Optional: Split by dots and take the first part
+    /**
+     * If the string contains a dot, the splitting operation proceeds as expected, and the first part is obtained.
+     * However, if the string does not contain a dot, the optional chaining operator prevents an error from occurring. Instead of throwing an error, it simply returns undefined because there is nothing to split.
+     */
     cb(
       null,
       filenameWithoutExtension +
@@ -26,6 +48,10 @@ const storage = multer.diskStorage({
         Math.ceil(Math.random() * 1e5) + // avoid rare name conflict
         fileExtension
     );
+    /**
+     * The cb function is used to indicate that the filename has been determined,
+     * and to pass that information back to multer. The cb function takes two arguments: an error object (or null if there is no error), and the filename.
+     */
   },
 });
 

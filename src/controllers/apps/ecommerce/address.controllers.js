0a64d66c66b7c5cf1cdff141086b8ee06ba22aae
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-26 18:11:27 +0530

    comments(server) : added comments to the address.controller.js file...

--- src/controllers/apps/ecommerce/address.controllers.js
+++ src/controllers/apps/ecommerce/address.controllers.js
@@ -26,14 +26,28 @@ const createAddress = asyncHandler(async (req, res) => {
 
 const getAllAddresses = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10 } = req.query;
+  /**
+   * Aggregate() - method is a feature of the MongoDB driver for Node.js that allows you to perform complex data processing operations on a collection.
+   * - In this case, the aggregate() method is being called on the Address model to create an aggregation pipeline.
+   *
+   * The aggregation operation is defined as an array of stages, and in this case, there is only one stage, which is a $match stage.
+   * - The pipeline consists of a single stage that uses the $match operator to filter the documents in the Address collection based on the owner field.
+   */
   const addressAggregation = Address.aggregate([
     {
+      // $match Stage:
       $match: {
+        // filteration criteria:
         owner: req.user._id,
       },
     },
   ]);
 
+  /**
+   * Pagination: Pagination is a technique for dividing a large number of addresses into smaller chunks, or pages.
+   * Pagination is important because if there are a large number of addresses in the database, returning all of them in a single response could cause performance issues and slow down the application.
+   * By using pagination, the code is able to limit the number of addresses that are returned in each response, making it easier to display the addresses in a user-friendly way.
+   */
   const addresses = await Address.aggregatePaginate(
     addressAggregation,
     getMongoosePaginationOptions({

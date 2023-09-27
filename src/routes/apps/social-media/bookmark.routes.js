1017174182b5eb4fcea8b9e60c323b1e0660591e
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-27 09:48:58 +0530

    test(server) : testing hte bookmark.routes.js file...

--- src/routes/apps/social-media/bookmark.routes.js
+++ src/routes/apps/social-media/bookmark.routes.js
@@ -1,10 +1,8 @@
 import { Router } from "express";
 import { verifyJWT } from "../../../middlewares/auth.middlewares.js";
 import { bookmarkUnBookmarkPost } from "../../../controllers/apps/social-media/bookmark.controllers.js";
-
 import { validate } from "../../../validators/validate.js";
 import { getBookMarkedPosts } from "../../../controllers/apps/social-media/post.controllers.js";
-
 import { mongoIdPathVariableValidator } from "../../../validators/common/mongodb.validators.js";
 
 const router = Router();

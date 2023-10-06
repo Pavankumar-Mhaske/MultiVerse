b4b447d670b0ade2311b32dcf65c29e7435c4f1a
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-06 18:03:47 +0530

    feat: chat changed to welcome route

--- examples/chat-app/web/src/components/PublicRoute.tsx
+++ examples/chat-app/web/src/components/PublicRoute.tsx
@@ -9,7 +9,7 @@ const PublicRoute: React.FC<{ children: ReactNode }> = ({ children }) => {
   const { token, user } = useAuth();
 
   // If there is a valid token and user ID, navigate the user to the chat page
-  if (token && user?._id) return <Navigate to="/chat" replace />;
+  if (token && user?._id) return <Navigate to="/welcome" replace />;
 
   // If no token or user ID exists, render the child components as they are
   return children;

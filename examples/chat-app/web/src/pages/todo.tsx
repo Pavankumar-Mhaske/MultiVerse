5dbb05a95145a1d7cfb68ce4a23185e6fba9b20f
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-06 18:05:29 +0530

    feat: added todo page

---
+++ examples/chat-app/web/src/pages/todo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function todo() {
+  return (
+    <div className="App">
+      <div className="flex flex-row">
+        <h1>Welcome to the TodoApp</h1>
+      </div>
+    </div>
+  );
+}
+
+export default todo;

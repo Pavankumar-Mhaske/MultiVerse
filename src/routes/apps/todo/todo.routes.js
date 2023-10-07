c0f64fcb57ebcf12990f9be60c333a9a6bd8837e
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-07 10:49:57 +0530

    comment: comment-out the entire file, causing overwrite issues

--- src/routes/apps/todo/todo.routes.js
+++ src/routes/apps/todo/todo.routes.js
@@ -1,47 +1,47 @@
-import { Router } from "express";
+// import { Router } from "express";
 
-import {
-  createTodo,
-  deleteTodo,
-  getAllTodos,
-  getTodoById,
-  toggleTodoDoneStatus,
-  updateTodo,
-} from "../../../controllers/apps/todo/todo.controllers.js";
-import {
-  createTodoValidator,
-  getAllTodosQueryValidators,
-  updateTodoValidator,
-} from "../../../validators/apps/todo/todo.validators.js";
+// import {
+//   createTodo,
+//   deleteTodo,
+//   getAllTodos,
+//   getTodoById,
+//   toggleTodoDoneStatus,
+//   updateTodo,
+// } from "../../../controllers/apps/todo/todo.controllers.js";
+// import {
+//   createTodoValidator,
+//   getAllTodosQueryValidators,
+//   updateTodoValidator,
+// } from "../../../validators/apps/todo/todo.validators.js";
 
-import { mongoIdPathVariableValidator } from "../../../validators/common/mongodb.validators.js";
+// import { mongoIdPathVariableValidator } from "../../../validators/common/mongodb.validators.js";
 
-import { validate } from "../../../validators/validate.js";
+// import { validate } from "../../../validators/validate.js";
 
-const router = Router();
+// const router = Router();
 
-router
-  .route("/")
-  .post(createTodoValidator(), validate, createTodo)
-  .get(getAllTodosQueryValidators(), validate, getAllTodos);
+// router
+//   .route("/")
+//   .post(createTodoValidator(), validate, createTodo)
+//   .get(getAllTodosQueryValidators(), validate, getAllTodos);
 
-router
-  .route("/:todoId")
-  .get(mongoIdPathVariableValidator("todoId"), validate, getTodoById)
-  .patch(
-    mongoIdPathVariableValidator("todoId"),
-    updateTodoValidator(),
-    validate,
-    updateTodo
-  )
-  .delete(mongoIdPathVariableValidator("todoId"), validate, deleteTodo);
+// router
+//   .route("/:todoId")
+//   .get(mongoIdPathVariableValidator("todoId"), validate, getTodoById)
+//   .patch(
+//     mongoIdPathVariableValidator("todoId"),
+//     updateTodoValidator(),
+//     validate,
+//     updateTodo
+//   )
+//   .delete(mongoIdPathVariableValidator("todoId"), validate, deleteTodo);
 
-router
-  .route("/toggle/status/:todoId")
-  .patch(
-    mongoIdPathVariableValidator("todoId"),
-    validate,
-    toggleTodoDoneStatus
-  );
+// router
+//   .route("/toggle/status/:todoId")
+//   .patch(
+//     mongoIdPathVariableValidator("todoId"),
+//     validate,
+//     toggleTodoDoneStatus
+//   );
 
-export default router;
+// export default router;

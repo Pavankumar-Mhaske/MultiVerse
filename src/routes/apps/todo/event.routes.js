7813ffa8924ae4e3089c077cb7c073c7eeaa24b0
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-07 15:05:06 +0530

    feat: added the evetns routes

---
+++ src/routes/apps/todo/event.routes.js
@@ -0,0 +1,37 @@
+import { Router } from "express";
+
+import {
+  createEvent,
+  getAllEvents,
+  getEventById,
+  deleteEvent,
+} from "../../../controllers/apps/todo/event.controllers.js";
+
+import {
+  createEventValidator,
+  updateEventValidator,
+  getAllEventsQueryValidators,
+} from "../../../validators/apps/todo/event.validators.js";
+
+import { mongoIdPathVariableValidator } from "../../../validators/common/mongodb.validators.js";
+
+import { validate } from "../../../validators/validate.js";
+
+const router = Router();
+
+router
+  .route("/")
+  .post(createEventValidator(), validate, createEvent)
+  .get(getAllEventsQueryValidators(), validate, getAllEvents);
+
+router
+  .route("/:eventId")
+  .get(
+    mongoIdPathVariableValidator("eventId"),
+
+    validate,
+    getEventById
+  )
+  .delete(mongoIdPathVariableValidator("eventId"), validate, deleteEvent);
+
+export default router;

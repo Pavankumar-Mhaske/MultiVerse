90cac76f75397cb3ebd2cfb0e65dca327d1f94da
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-07 10:48:44 +0530

    comment: comment-out the entire file, causing overwrite issues

--- src/models/apps/todo/todo.models.js
+++ src/models/apps/todo/todo.models.js
@@ -1,22 +1,22 @@
-import mongoose, { Schema } from "mongoose";
+// import mongoose, { Schema } from "mongoose";
 
-const todoSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-      index: true,
-    },
-    description: {
-      type: String,
-      default: "",
-    },
-    isComplete: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  { timestamps: true }
-);
+// const todoSchema = new Schema(
+//   {
+//     title: {
+//       type: String,
+//       required: true,
+//       index: true,
+//     },
+//     description: {
+//       type: String,
+//       default: "",
+//     },
+//     isComplete: {
+//       type: Boolean,
+//       default: false,
+//     },
+//   },
+//   { timestamps: true }
+// );
 
-export const Todo = mongoose.model("Todo", todoSchema);
+// // export const Todo = mongoose.model("Todo", todoSchema);

529e8db7f0a30a8766dba9b38063c8c8af2182bf
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-07 09:49:07 +0530

    feat: added event.models

---
+++ src/models/apps/todo/event.models.js
@@ -0,0 +1,40 @@
+/**
+ * Importing mongoose
+ */
+const mongoose = require("mongoose");
+const { User } = require("../auth/user.models.js");
+/**
+ * Destructuring from mongoose
+ *      - Schema Constructor
+ *      - model method
+ */
+const { Schema, model } = mongoose;
+
+const EventSchema = new Schema(
+  {
+    reminderMsg: {
+      type: String,
+      required: [true, "Reminder message is required"],
+    },
+    remindAt: {
+      // type: Date ,
+      type: String,
+      required: [true, "Reminder time is required"],
+    },
+    isReminded: {
+      type: Boolean,
+      default: false,
+    },
+    // Keeping the reference of the use who created the reminder
+    author: {
+      type: Schema.Types.ObjectId, // This is a reference to the User model
+      ref: "User",
+      required: [true, "User is required to store Event"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("Event", EventSchema);

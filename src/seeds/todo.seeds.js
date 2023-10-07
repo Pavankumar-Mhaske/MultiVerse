1e73f8a52b7af1c3c832c16af6e0eaf6cff26a2a
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-07 08:07:56 +0530

    comment: added comments in the todo.seeds.js

--- src/seeds/todo.seeds.js
+++ src/seeds/todo.seeds.js
@@ -6,6 +6,12 @@ import { TODOS_COUNT } from "./_constants.js";
 
 // Generate random todos
 const todos = new Array(TODOS_COUNT).fill("_").map(() => ({
+  /* generates a random sentence using the faker library's lorem module.
+   * faker - is a JavaScript library
+   * lorem - is a module of the faker library
+   * sentence - is a function of the lorem module
+   * min and max - are options
+   */
   title: faker.lorem.sentence({ min: 3, max: 5 }),
   description: faker.lorem.paragraph({
     min: 10,

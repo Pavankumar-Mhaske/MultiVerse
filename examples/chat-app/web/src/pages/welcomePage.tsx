17409b157c99fbeba07f09278df548412da13efb
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-08 15:34:53 +0530

    chore: added some styling on the buttons of the welcomePage

--- examples/chat-app/web/src/pages/welcomePage.tsx
+++ examples/chat-app/web/src/pages/welcomePage.tsx
@@ -16,10 +16,20 @@ function Welcome() {
 
   return (
     <div className="App">
-      <div className="flex flex-row">
-        <h1>Choose an App</h1>
-        <button onClick={handleTodoApp}>TodoApp </button>
-        <button onClick={handleChatApp}>ChatApp </button>
+      <div className="flex flex-row items-center">
+        <h1 className="mr-4">Choose an App</h1>
+        <button
+          className="px-4 py-2 rounded-md bg-blue-500 text-white mr-2"
+          onClick={handleTodoApp}
+        >
+          TodoApp
+        </button>
+        <button
+          className="px-4 py-2 rounded-md bg-green-500 text-white"
+          onClick={handleChatApp}
+        >
+          ChatApp
+        </button>
       </div>
     </div>
   );

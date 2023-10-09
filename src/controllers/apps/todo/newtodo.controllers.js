e174869a96e041104b6e9f737815d7e6b1d50f84
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-09 09:49:01 +0530

    feat: added the getUserTodos utility

--- src/controllers/apps/todo/newtodo.controllers.js
+++ src/controllers/apps/todo/newtodo.controllers.js
@@ -135,6 +135,59 @@ const getAllTodos = async (req, res) => {
 };
 
 /**
+ * getUserTodos() - Asynchronous Function
+ *      - Destructures the input received in req.params.
+ *      - Validated if userId is received.
+ *      - Validated if userId received is of type string.
+ *      - Fetches the user with respect to userId and populate todos field. (Asynchronous operation - find())
+ */
+
+const getUserTodos = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    if (userId) {
+      if (typeof userId === "string") {
+        const user = await Todo.find({ author: userId });
+        console.log(user);
+
+        if (user) {
+          return res.status(200).json({
+            success: true,
+            message: "User Todos fetched successfully",
+            user: user,
+            status: 200,
+          });
+        } else {
+          return res.status(400).json({
+            success: false,
+            message: "User not found",
+          });
+        }
+      } else {
+        return res.status(400).json({
+          success: false,
+          message: "userId should be of type string",
+        });
+      }
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: "userId is required",
+      });
+    }
+  } catch (error) {
+    console.log("Error in get user todos controller");
+    console.log("ERROR: ", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
+/**
  * getTodo() - Asynchronous Function
  *      - Destructures the input received in req.params.
  *      - Validated if todoId is received.
@@ -494,6 +547,7 @@ const searchTodos = async (req, res) => {
 export {
   createTodo,
   getAllTodos,
+  getUserTodos,
   getTodoById,
   updateTodo,
   deleteTodo,

6af43895e5b639b7e36896d3c3bf2c935f18aa52
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-10-09 11:18:12 +0530

    chore: for the testing purpose the server request limits increased

--- src/app.js
+++ src/app.js
@@ -94,8 +94,8 @@ app.use(
 
 // Rate limiter to avoid misuse of the service and avoid cost spikes
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 500, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
+  windowMs: 1 * 60 * 1000, // 15 minutes
+  max: 50000, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   handler: (_, __, ___, options) => {
